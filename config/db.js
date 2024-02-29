3e70e58d097e078f293efe92df19809c4925d539
chelseaavrisamukti <chelseamukti9@gmail.com>
2024-02-29 08:50:48 +0700

    first commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: 'localhost',
     USER: 'root',
     PASSWORD: '',
-    DB: 'quizapi',
+    DB: 'quizapipsaj',
     dialect: 'mysql',
     pool: {
         max: 5,
--- router/quiz.js
+++ router/quiz.js
@@ -1,8 +1,8 @@
 const quizController = require('../controllers/quiz');
 const router = require('express').Router();
 
-router.post('./', quizController.create);
-router.get('./', quizController.getAll);
+router.post('/', quizController.create);
+router.get('/', quizController.getAll);
 router.get('/:id', quizController.findOne);
 router.put('/:id', quizController.update);
 router.delete('/:id', quizController.delete);
